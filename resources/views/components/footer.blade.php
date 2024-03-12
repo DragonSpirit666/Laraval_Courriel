a4a2d4faf277ef01e47231ff50a9e4268cb08c42
DragonSpirit666 <zacharydtremblay@gmail.com>
2024-03-12 11:39:29 -0400

    awdawd

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
 
 
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+<footer class="fixed bottom-0 w-full bg-white shadow dark:bg-gray-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">Cstjean™</a>. All Rights Reserved.
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{__('components.copyright.year')}} <a href="https://flowbite.com/" class="hover:underline">{{__('components.copyright.company')}}</a>. {{__('components.copyright.text')}}
         </span>
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">Zachary Deschênes-Tremblay, Justin Morand
         </span>

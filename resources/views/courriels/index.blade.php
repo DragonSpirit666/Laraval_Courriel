dfb05b40f544e95e66bd5de55856c5d82f90e7e6
V0rtek laptop <justinmorand@outlook.com>
2024-03-12 11:24:56 -0400

    safasd

--- resources/views/courriels/index.blade.php
+++ resources/views/courriels/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot name="titre" class="text-center">
         <div class="flex flex-col">
-            <h1 class="text-6xl font-bold text-center text-gray-800">Welcome</h1>
+            <h1 class="text-6xl font-bold text-center text-gray-800">Courriel</h1>
             <div class="pt-6 self-center text-base leading-7 text-gray-700 max-w-xl">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 At, consequuntur debitis deserunt eaque eum facere inventore,

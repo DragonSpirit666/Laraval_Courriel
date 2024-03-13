decced8379f1fced03e0eba804f08e1ccf3cb99e
V0rtek laptop <justinmorand@outlook.com>
2024-03-13 12:19:16 -0400

    oupssadasda

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CourrielController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
 /*
 |--------------------------------------------------------------------------
